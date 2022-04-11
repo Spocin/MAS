00a5e1a9358399de17dba445bb684a1cd8a25e9d
Spocin <spociomaniak@gmail.com>
2022-04-11 11:22:28 +0200

    Implemented Person.java (Optional field, constructor overload)

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,30 @@
 public class Person {
 
+    private String firstName;
+    private String secondName = null;
+    private String lastName;
+    private String pesel;
+    private String email;
 
+    private Patient patient;
+    private Doctor doctor;
 
+    public Person (String firstName, String secondName, String lastName, String pesel, String email, Patient patient, Doctor doctor) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.pesel = pesel;
+        this.email = email;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
+
+    public Person (String firstName, String lastName, String pesel, String email, Patient patient, Doctor doctor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.pesel = pesel;
+        this.email = email;
+        this.patient = patient;
+        this.doctor = doctor;
+    }
 }
