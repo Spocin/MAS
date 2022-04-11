c36296c70beda196babd4baf470032fa3e3517ce
Spocin <spociomaniak@gmail.com>
2022-04-11 11:35:41 +0200

    Implemented Language fields and constructor.

--- src/main/java/Language.java
+++ src/main/java/Language.java
@@ -1,2 +1,9 @@
 public class Language {
+    private String name;
+    private String code;
+
+    public Language (String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
