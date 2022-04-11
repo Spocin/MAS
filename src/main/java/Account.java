b3c6de5a3d1300e22c74a49cf6d17af8636f896a
Spocin <spociomaniak@gmail.com>
2022-04-11 11:05:07 +0200

    Implemented Account.java (Extension, fields, constructor).

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,15 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Account {
+
+    private static ArrayList<Account> accounts = new ArrayList<>();
+    private final LocalDateTime creationTime;
+
+    private Person person;
+
+    public Account (Person person) {
+        this.creationTime = LocalDateTime.now();
+        this.person = person;
+    }
 }
