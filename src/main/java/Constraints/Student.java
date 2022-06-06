7af4b5cc10b024ff29dc3646fc55314a08ffc5d8
Spocin <7z\kt\h.TS:87izb5\*dM'|7h>
2022-06-06 12:24:06 +0200

    Implemented unique constraint.

--- src/main/java/Constraints/Student.java
+++ src/main/java/Constraints/Student.java
@@ -1,12 +1,18 @@
 package Constraints;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Student {
     public final static float maxStudentsTuitionBeforeRiotsStart = 1500;
+    public static Set<String> eski = new HashSet<>();
 
     private float tuition;
+    private String eska;
 
-    public Student (float tuition) throws Exception {
+    public Student (float tuition, String eska) throws Exception {
         setTuition(tuition);
+        setEska(eska);
     }
 
     public void setTuition (float tuition) throws Exception {
@@ -19,5 +25,18 @@ public class Student {
         }
 
         this.tuition = tuition;
+    }
+
+    public void setEska (String eska) throws Exception {
+        if (!eska.startsWith("s")) {
+            throw new Exception("Eska must start with letter s");
+        }
+
+        if (eski.contains(eska)) {
+            throw new Exception("Such ESKA already exists but must be unique");
+        }
+
+        this.eska = eska;
+        eski.add(eska);
     }
 }
